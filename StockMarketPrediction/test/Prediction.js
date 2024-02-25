df13056133f0e56089a672923b7a7aadf8223d39
berk gümüş <63772343+berk420@users.noreply.github.com>
2024-02-25 22:08:03 +0300

    Update

--- StockMarketPrediction/test/Prediction.js
+++ StockMarketPrediction/test/Prediction.js
@@ -2,102 +2,86 @@ const {By, Builder } = require("selenium-webdriver");
 require("chromedriver"); 
 const { describe, it } = require('mocha');
 const addContext = require('mochawesome/addContext');
+const {toplam_oazkayank,Dönem_Net_Kar_Zararı,Toplam_Kısa_Vadeli_Yükümlülükler,Toplam_Uzun_Vadeli_Yükümlülükler,Nakit_ve_Nakit_Benzerleri,ticari_alacaklar} = require("./financial_statement_values.js");
 
-const stocks = ["TUPRS"];
 
-describe("Stockmarket", async function () {
-    //const browsers = ["chrome", "firefox", "MicrosoftEdge"];
-    //const stocks = ["TUPRS","KCHOL","TKNSA","REEDR"];
 
-    stocks.forEach(stock =>{
+const stocks_petrol = ["TUPRS"];
+const stocks_demirçelik = ["KCAER"];
+
+
+describe("Petrol", async function () {
+
+    stocks_petrol.forEach(stock =>{
     describe(stock,async function() {
-      let fiyatKazancOrani="aa";
-      let ss = "dd";
+
+      it("make sure the data", async function(){
+
+        let driver = await new Builder().forBrowser('chrome').build();
+
+        await driver.get(`https://fintables.com/sirketler/${stock}/finansal-tablolar/bilanco`); 
+
+        driver.manage().window().maximize();
+
+        let toplam_oazkayank=parseInt(await toplam_oazkayank_value(driver));
+        
+        await driver.quit();
+      })
 
 
-      
-    
       it("fiyatKazancOrani", async function(){
 
         let driver = await new Builder().forBrowser('chrome').build();
 
-        try {
+        await driver.get(`https://fintables.com/sirketler/${stock}/finansal-tablolar/bilanco`); 
+
+        let toplam_oazkayank_value=parseInt(await toplam_oazkayank(driver));
+        let Dönem_Net_Kar_Zararı_value=parseInt(await Dönem_Net_Kar_Zararı(driver));
+        let Toplam_Kısa_Vadeli_Yükümlülükler_value=parseInt(await Toplam_Kısa_Vadeli_Yükümlülükler(driver));
+        let Toplam_Uzun_Vadeli_Yükümlülükler_value=parseInt(await Toplam_Uzun_Vadeli_Yükümlülükler(driver));
+        let Nakit_ve_Nakit_Benzerleri_value=parseInt(await Nakit_ve_Nakit_Benzerleri(driver));
+        let ticari_alacaklar_value=parseInt(await ticari_alacaklar(driver));
+
+        let tüm_borçlar=Toplam_Kısa_Vadeli_Yükümlülükler_value+Toplam_Uzun_Vadeli_Yükümlülükler_value;
+        console.log("Toplam_Kısa_Vadeli_Yükümlülükler_value+Toplam_Uzun_Vadeli_Yükümlülükler_value=tümborçlar:",tüm_borçlar);
+    
+        let fiyatKazancOrani = Nakit_ve_Nakit_Benzerleri_value / Dönem_Net_Kar_Zararı_value;
+        console.log(" Nakit_ve_Nakit_Benzerleri_value / Dönem_Net_Kar_Zararı_value=fiyat kazanç oranı:",fiyatKazancOrani);
+    
+        const fiyatKazancOraniEsik = 15;//bu bence çok yüksek bir oran
+        if (fiyatKazancOrani < fiyatKazancOraniEsik) { //düzenlenecek
+          addContext(this, `fiyatKazancOrani < fiyatKazancOraniEsik true ${stock}`);
+
+
+        }
+        else{
+          fiyatKazancOrani=`fiyatKazancOrani < fiyatKazancOraniEsik false ${stock}`;
+        }
+    
+      
+        await driver.quit();
+
+          
+      })
+
+      it("borcOzkaynakOrani", async function(){
+
+        let driver = await new Builder().forBrowser('chrome').build();
+
 
           await driver.get(`https://fintables.com/sirketler/${stock}/finansal-tablolar/bilanco`); 
 
           driver.manage().window().maximize();
-      
-          await new Promise(resolve => setTimeout(resolve, 1000));
-      
-          const toplam_ozkaynak=await driver.findElement(By.xpath(`//*[@id="TUPRS"]/div[5]/div[1]/div/main/div/div/div[3]/div[4]/table[2]/tbody[5]/tr[12]/td[2]/div/span[2]`));
-          toplam_ozkaynak.getText().then(result => {
-            toplam_oazkayank_value=result;
-              })
-          await new Promise(resolve => setTimeout(resolve, 1000));
-      
-          let Dönem_Net_Kar_Zararı_value;
-          const Dönem_Net_Kar_Zararı=await driver.findElement(By.xpath(`//*[@id="TUPRS"]/div[5]/div[1]/div/main/div/div/div[3]/div[4]/table[2]/tbody[5]/tr[10]/td[2]/div/span[2]`));
-          Dönem_Net_Kar_Zararı.getText().then(result => {
-            Dönem_Net_Kar_Zararı_value=result;
-              })
-          await new Promise(resolve => setTimeout(resolve, 1000));
-      
-          let Toplam_Kısa_Vadeli_Yükümlülükler_value;
-          const Toplam_Kısa_Vadeli_Yükümlülükler=await driver.findElement(By.xpath(`//*[@id="TUPRS"]/div[5]/div[1]/div/main/div/div/div[3]/div[4]/table[2]/tbody[3]/tr[11]/td[2]/div/span[2]`));
-          Toplam_Kısa_Vadeli_Yükümlülükler.getText().then(result => {
-            Toplam_Kısa_Vadeli_Yükümlülükler_value=result;
-              })
-          await new Promise(resolve => setTimeout(resolve, 1000));
-      
-          let Toplam_Uzun_Vadeli_Yükümlülükler_value;
-          const Toplam_Uzun_Vadeli_Yükümlülükler=await driver.findElement(By.xpath(`//*[@id="TUPRS"]/div[5]/div[1]/div/main/div/div/div[3]/div[4]/table[2]/tbody[3]/tr[11]/td[2]/div/span[2]`));
-          Toplam_Uzun_Vadeli_Yükümlülükler.getText().then(result => {
-            Toplam_Uzun_Vadeli_Yükümlülükler_value=result;
-              })
-          await new Promise(resolve => setTimeout(resolve, 1000));
-      
-          let Nakit_ve_Nakit_Benzerleri_value;
-          const Nakit_ve_Nakit_Benzerleri =await driver.findElement(By.xpath(`//*[@id="TUPRS"]/div[5]/div[1]/div/main/div/div/div[3]/div[4]/table[2]/tbody[1]/tr[2]/td[2]/div/span[2]`));
-          Nakit_ve_Nakit_Benzerleri.getText().then(result => {
-            Nakit_ve_Nakit_Benzerleri_value=result;
-              })
-          await new Promise(resolve => setTimeout(resolve, 1000));
-                          
-          let ticari_alacaklar_value;
-          const ticari_alacaklar=await driver.findElement(By.xpath(`//*[@id="TUPRS"]/div[5]/div[1]/div/main/div/div/div[3]/div[4]/table[2]/tbody[1]/tr[4]/td[2]/div/span[2]`));
-          ticari_alacaklar.getText().then(result => {
-            ticari_alacaklar_value=result;
-              })
-          await new Promise(resolve => setTimeout(resolve, 1000));
-      
-      
-          toplam_oazkayank_value=parseInt(toplam_oazkayank_value);
-          Dönem_Net_Kar_Zararı_value=parseInt(Dönem_Net_Kar_Zararı_value);
-          Toplam_Kısa_Vadeli_Yükümlülükler_value=parseInt(Toplam_Kısa_Vadeli_Yükümlülükler_value);
-          Toplam_Uzun_Vadeli_Yükümlülükler_value=parseInt(Toplam_Uzun_Vadeli_Yükümlülükler_value);
-          Nakit_ve_Nakit_Benzerleri_value=parseInt(Nakit_ve_Nakit_Benzerleri_value);
-          ticari_alacaklar_value=parseInt(ticari_alacaklar_value)
-      
 
-          let tüm_borçlar=Toplam_Kısa_Vadeli_Yükümlülükler_value+Toplam_Uzun_Vadeli_Yükümlülükler_value;
-          console.log("Toplam_Kısa_Vadeli_Yükümlülükler_value+Toplam_Uzun_Vadeli_Yükümlülükler_value=tümborçlar:",tüm_borçlar);
+          await toplam_oazkayank_value(driver);
+          await Toplam_Kısa_Vadeli_Yükümlülükler_value(driver);
+          await Toplam_Uzun_Vadeli_Yükümlülükler_value (driver);
       
-          fiyatKazancOrani = Nakit_ve_Nakit_Benzerleri_value / Dönem_Net_Kar_Zararı_value;
-          console.log(" Nakit_ve_Nakit_Benzerleri_value / Dönem_Net_Kar_Zararı_value=fiyat kazanç oranı:",fiyatKazancOrani);
-      
-          const fiyatKazancOraniEsik = 15;//bu bence çok yüksek bir oran
-          if (fiyatKazancOrani < fiyatKazancOraniEsik) { //düzenlenecek
-            console.log();
-            addContext(this, `fiyatKazancOrani < fiyatKazancOraniEsik true ${stock}`);
+          let toplam_oazkayank_value=parseInt(await toplam_oazkayank(driver));
+          let Toplam_Kısa_Vadeli_Yükümlülükler_value=parseInt(await Toplam_Kısa_Vadeli_Yükümlülükler(driver));
+          let Toplam_Uzun_Vadeli_Yükümlülükler_value=parseInt(await Toplam_Uzun_Vadeli_Yükümlülükler(driver));
+          let Nakit_ve_Nakit_Benzerleri_value=parseInt(await Nakit_ve_Nakit_Benzerleri(driver));
 
-          }
-          else{
-
-            console.log(`fiyatKazancOrani < fiyatKazancOraniEsik false ${stock}`);
-
-            fiyatKazancOrani=`fiyatKazancOrani < fiyatKazancOraniEsik false ${stock}`;
-          }
-      
           let borcOzkaynakOrani = Toplam_Kısa_Vadeli_Yükümlülükler_value + Toplam_Uzun_Vadeli_Yükümlülükler_value / toplam_oazkayank_value;
       
           const borcOzkaynakOraniEsik = 0.5;
@@ -109,40 +93,148 @@ describe("Stockmarket", async function () {
             console.log(`borcOzkaynakOrani < borcOzkaynakOraniEsik false ${stock}`);
           }
       
-          const likitOranEsik = 2;
-          
-          let likitoran=(Nakit_ve_Nakit_Benzerleri_value+ticari_alacaklar_value)/Toplam_Kısa_Vadeli_Yükümlülükler_value;
-          
-          //Likit oran, bir şirketin kısa vadeli borçlarını ödeme yeteneğini gösteren bir finansal orandır. 
-          
-          if(likitOranEsik < likitoran){
-            console.log(`likitOranEsik < likitoran true ${stock}`);
-          }
-          else{
-            console.log(`likitOranEsik < likitoran false${stock}`);
-          }
-
-          
-         it(String("fiyatKazancOrani"), async function(){})
           await driver.quit();
-      }
-      catch(err) {
-          console.log(err);
-          driver.quit();
-      }
           
       })
 
+      it("likitoran", async function(){
 
+        let driver = await new Builder().forBrowser('chrome').build();
 
+        await driver.get(`https://fintables.com/sirketler/${stock}/finansal-tablolar/bilanco`); 
 
+        driver.manage().window().maximize();
 
+        let toplam_oazkayank_value=parseInt(await toplam_oazkayank(driver));
+        let Toplam_Kısa_Vadeli_Yükümlülükler_value=parseInt(await Toplam_Kısa_Vadeli_Yükümlülükler(driver));
+        let Toplam_Uzun_Vadeli_Yükümlülükler_value=parseInt(await Toplam_Uzun_Vadeli_Yükümlülükler(driver));
+        let Nakit_ve_Nakit_Benzerleri_value=parseInt(await Nakit_ve_Nakit_Benzerleri(driver));
 
+        const likitOranEsik = 2;
+        
+        let likitoran=(Nakit_ve_Nakit_Benzerleri_value+ticari_alacaklar_value)/Toplam_Kısa_Vadeli_Yükümlülükler_value;
+        addContext(`Likit oran, bir şirketin kısa vadeli borçlarını ödeme yeteneğini gösteren bir finansal orandır`);
+        
+        if(likitOranEsik < likitoran){
+          console.log(`likitOranEsik < likitoran true ${stock}`);
+        }
+        else{
+          console.log(`likitOranEsik < likitoran false${stock}`);
+        }
 
-
-    
+        await driver.quit();
+          
+      })
    
     })
   })
 })
 
+
+describe("demir çelik", async function () {
+  //const browsers = ["chrome", "firefox", "MicrosoftEdge"];
+  //const stocks = ["TUPRS","KCHOL","TKNSA","REEDR"];
+
+  stocks_demirçelik.forEach(stock =>{
+  describe(stock,async function() {
+
+    it("fiyatKazancOrani", async function(){
+
+      let driver = await new Builder().forBrowser('chrome').build();
+
+      await driver.get(`https://fintables.com/sirketler/${stock}/finansal-tablolar/bilanco`); 
+
+      let toplam_oazkayank_value=parseInt(await toplam_oazkayank(driver));
+      let Dönem_Net_Kar_Zararı_value=parseInt(await Dönem_Net_Kar_Zararı(driver));
+      let Toplam_Kısa_Vadeli_Yükümlülükler_value=parseInt(await Toplam_Kısa_Vadeli_Yükümlülükler(driver));
+      let Toplam_Uzun_Vadeli_Yükümlülükler_value=parseInt(await Toplam_Uzun_Vadeli_Yükümlülükler(driver));
+      let Nakit_ve_Nakit_Benzerleri_value=parseInt(await Nakit_ve_Nakit_Benzerleri(driver));
+      let ticari_alacaklar_value=parseInt(await ticari_alacaklar(driver));
+
+      let tüm_borçlar=Toplam_Kısa_Vadeli_Yükümlülükler_value+Toplam_Uzun_Vadeli_Yükümlülükler_value;
+      console.log("Toplam_Kısa_Vadeli_Yükümlülükler_value+Toplam_Uzun_Vadeli_Yükümlülükler_value=tümborçlar:",tüm_borçlar);
+  
+      let fiyatKazancOrani = Nakit_ve_Nakit_Benzerleri_value / Dönem_Net_Kar_Zararı_value;
+      console.log(" Nakit_ve_Nakit_Benzerleri_value / Dönem_Net_Kar_Zararı_value=fiyat kazanç oranı:",fiyatKazancOrani);
+  
+      const fiyatKazancOraniEsik = 15;//bu bence çok yüksek bir oran
+      if (fiyatKazancOrani < fiyatKazancOraniEsik) { //düzenlenecek
+        addContext(this, `fiyatKazancOrani < fiyatKazancOraniEsik true ${stock}`);
+
+
+      }
+      else{
+        fiyatKazancOrani=`fiyatKazancOrani < fiyatKazancOraniEsik false ${stock}`;
+      }
+  
+    
+      await driver.quit();
+
+        
+    })
+
+    it("borcOzkaynakOrani", async function(){
+
+      let driver = await new Builder().forBrowser('chrome').build();
+
+
+        await driver.get(`https://fintables.com/sirketler/${stock}/finansal-tablolar/bilanco`); 
+
+        driver.manage().window().maximize();
+
+        await toplam_oazkayank_value(driver);
+        await Toplam_Kısa_Vadeli_Yükümlülükler_value(driver);
+        await Toplam_Uzun_Vadeli_Yükümlülükler_value (driver);
+    
+        let toplam_oazkayank_value=parseInt(await toplam_oazkayank(driver));
+        let Toplam_Kısa_Vadeli_Yükümlülükler_value=parseInt(await Toplam_Kısa_Vadeli_Yükümlülükler(driver));
+        let Toplam_Uzun_Vadeli_Yükümlülükler_value=parseInt(await Toplam_Uzun_Vadeli_Yükümlülükler(driver));
+        let Nakit_ve_Nakit_Benzerleri_value=parseInt(await Nakit_ve_Nakit_Benzerleri(driver));
+
+        let borcOzkaynakOrani = Toplam_Kısa_Vadeli_Yükümlülükler_value + Toplam_Uzun_Vadeli_Yükümlülükler_value / toplam_oazkayank_value;
+    
+        const borcOzkaynakOraniEsik = 0.5;
+    
+        if (borcOzkaynakOrani < borcOzkaynakOraniEsik) {
+          console.log(`borcOzkaynakOrani < borcOzkaynakOraniEsik true ${stock}`);
+        }
+        else{
+          console.log(`borcOzkaynakOrani < borcOzkaynakOraniEsik false ${stock}`);
+        }
+    
+        await driver.quit();
+        
+    })
+
+    it("likitoran", async function(){
+
+      let driver = await new Builder().forBrowser('chrome').build();
+
+      await driver.get(`https://fintables.com/sirketler/${stock}/finansal-tablolar/bilanco`); 
+
+      driver.manage().window().maximize();
+
+      let toplam_oazkayank_value=parseInt(await toplam_oazkayank(driver));
+      let Toplam_Kısa_Vadeli_Yükümlülükler_value=parseInt(await Toplam_Kısa_Vadeli_Yükümlülükler(driver));
+      let Toplam_Uzun_Vadeli_Yükümlülükler_value=parseInt(await Toplam_Uzun_Vadeli_Yükümlülükler(driver));
+      let Nakit_ve_Nakit_Benzerleri_value=parseInt(await Nakit_ve_Nakit_Benzerleri(driver));
+
+      const likitOranEsik = 2;
+      
+      let likitoran=(Nakit_ve_Nakit_Benzerleri_value+ticari_alacaklar_value)/Toplam_Kısa_Vadeli_Yükümlülükler_value;
+      addContext(`Likit oran, bir şirketin kısa vadeli borçlarını ödeme yeteneğini gösteren bir finansal orandır`);
+      
+      if(likitOranEsik < likitoran){
+        console.log(`likitOranEsik < likitoran true ${stock}`);
+      }
+      else{
+        console.log(`likitOranEsik < likitoran false${stock}`);
+      }
+
+      await driver.quit();
+        
+    })
+})
+})
+
+})
---
+++ StockMarketPrediction/test/financial_statement_values.js
@@ -0,0 +1,87 @@
+//module.exportsconst { Base_URL, Second_URL, SuperAdmin_email, SuperAdmin_password, TenantAdmin_email, TenantAdmin_password, User_email, User_password} = require("./variables.js");
+const {By, Builder,sendKeys,Key, WebElement,until } = require("selenium-webdriver");
+module.exports = {
+    toplam_oazkayank:async function(driver) {
+        driver.manage().window().maximize();
+      
+        let toplam_oazkayank_value;
+        await new Promise(resolve => setTimeout(resolve, 1000));
+    
+        const toplam_ozkaynak=await driver.findElement(By.xpath(`//*[@id="TUPRS"]/div[5]/div[1]/div/main/div/div/div[3]/div[4]/table[2]/tbody[5]/tr[12]/td[2]/div/span[2]`));
+        
+        toplam_ozkaynak.getText().then(result => {
+            toplam_oazkayank_value=result;
+        })
+
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        return toplam_oazkayank_value;
+    },
+
+    Dönem_Net_Kar_Zararı:async function(driver) {
+        driver.manage().window().maximize();
+
+        let Dönem_Net_Kar_Zararı_value;
+
+        const Dönem_Net_Kar_Zararı=await driver.findElement(By.xpath(`//*[@id="TUPRS"]/div[5]/div[1]/div/main/div/div/div[3]/div[4]/table[2]/tbody[5]/tr[10]/td[2]/div/span[2]`));
+        Dönem_Net_Kar_Zararı.getText().then(result => {
+          Dönem_Net_Kar_Zararı_value=result;
+            })
+        await new Promise(resolve => setTimeout(resolve, 1000));       
+
+        return Dönem_Net_Kar_Zararı_value;
+    },
+
+    
+    Toplam_Kısa_Vadeli_Yükümlülükler:async function(driver) {
+        driver.manage().window().maximize();
+        let Toplam_Kısa_Vadeli_Yükümlülükler_value;
+        const Toplam_Kısa_Vadeli_Yükümlülükler=await driver.findElement(By.xpath(`//*[@id="TUPRS"]/div[5]/div[1]/div/main/div/div/div[3]/div[4]/table[2]/tbody[3]/tr[11]/td[2]/div/span[2]`));
+        Toplam_Kısa_Vadeli_Yükümlülükler.getText().then(result => {
+          Toplam_Kısa_Vadeli_Yükümlülükler_value=result;
+            })
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        return Toplam_Kısa_Vadeli_Yükümlülükler_value;
+    },
+
+    Toplam_Uzun_Vadeli_Yükümlülükler:async function(driver) {
+        driver.manage().window().maximize();
+        let Toplam_Uzun_Vadeli_Yükümlülükler_value;
+        const Toplam_Uzun_Vadeli_Yükümlülükler=await driver.findElement(By.xpath(`//*[@id="TUPRS"]/div[5]/div[1]/div/main/div/div/div[3]/div[4]/table[2]/tbody[3]/tr[11]/td[2]/div/span[2]`));
+        Toplam_Uzun_Vadeli_Yükümlülükler.getText().then(result => {
+          Toplam_Uzun_Vadeli_Yükümlülükler_value=result;
+            })
+        await new Promise(resolve => setTimeout(resolve, 1000));
+
+        return Toplam_Uzun_Vadeli_Yükümlülükler_value;
+    },
+
+    Nakit_ve_Nakit_Benzerleri:async function(driver) {
+        driver.manage().window().maximize();
+
+        let Nakit_ve_Nakit_Benzerleri_value;
+        const Nakit_ve_Nakit_Benzerleri =await driver.findElement(By.xpath(`//*[@id="TUPRS"]/div[5]/div[1]/div/main/div/div/div[3]/div[4]/table[2]/tbody[1]/tr[2]/td[2]/div/span[2]`));
+        Nakit_ve_Nakit_Benzerleri.getText().then(result => {
+          Nakit_ve_Nakit_Benzerleri_value=result;
+            })
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        
+        return Nakit_ve_Nakit_Benzerleri_value;
+    },
+
+    ticari_alacaklar:async function(driver) {
+        driver.manage().window().maximize();
+
+        let ticari_alacaklar_value;
+        const ticari_alacaklar=await driver.findElement(By.xpath(`//*[@id="TUPRS"]/div[5]/div[1]/div/main/div/div/div[3]/div[4]/table[2]/tbody[1]/tr[4]/td[2]/div/span[2]`));
+        ticari_alacaklar.getText().then(result => {
+          ticari_alacaklar_value=result;
+            })
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        
+        return ticari_alacaklar_value;
+    },
+
+
+  };
