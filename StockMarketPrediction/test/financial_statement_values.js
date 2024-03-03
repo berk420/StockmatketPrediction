a29d20036db47a1de1b342a1603199e70070efed
berk gümüş <63772343+berk420@users.noreply.github.com>
2024-03-03 21:14:16 +0300

    trying to make 2d array of any balance sheet item

--- StockMarketPrediction/test/financial_statement_values.js
+++ StockMarketPrediction/test/financial_statement_values.js
@@ -1,6 +1,82 @@
 //module.exportsconst { Base_URL, Second_URL, SuperAdmin_email, SuperAdmin_password, TenantAdmin_email, TenantAdmin_password, User_email, User_password} = require("./variables.js");
 const {By, Builder,sendKeys,Key, WebElement,until } = require("selenium-webdriver");
+
 module.exports = {
+
+  toplam_oazkayank_all:async function(driver,stock) {
+    await driver.manage().window().maximize();
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    await driver.navigate().refresh();
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+
+
+    const element = await driver.findElement(By.xpath(`//*[@id="TUPRS"]/div[5]/div[1]/div/main/div/div/div[3]/div[4]/table[1]/thead/tr/th[2]/div/div`));
+    const tumStringler = element.innerHTML;
+    
+    const regex = /(<.*?>)(.*?)(<.*?>)/g;
+    const matches = tumStringler.match(regex);
+    
+    for (const match of matches) {
+      console.log(match[2]); // Her bir stringi ayrı ayrı ekrana yazdırır.
+    }
+    
+
+
+
+    let toplam_oazkayank_all_son_tarih_time;
+
+    const toplam_ozkaynak_all_sontarih=await driver.findElement(By.xpath(`//*[@id="TUPRS"]/div[5]/div[1]/div/main/div/div/div[3]/div[4]/table[1]/thead/tr/th[2]/div/div`));
+    //const toplam_ozkaynak_all_sontarih=await driver.findElement(By.xpath(`//*[@id="TUPRS"]/div[5]/div[1]/div/main/div/div/div[3]/div[4]/table[1]/thead/tr/th[2]/div/div`));
+    
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    console.log(toplam_ozkaynak_all_sontarih.textContent);
+    console.log(toplam_ozkaynak_all_sontarih.allData);
+    console.log(toplam_ozkaynak_all_sontarih.getText);
+    //debugger;
+    console.log(toplam_ozkaynak_all_sontarih.WebElement);
+    console.log(toplam_ozkaynak_all_sontarih.exports);
+    console.log(toplam_ozkaynak_all_sontarih.result);
+
+    console.log(toplam_ozkaynak_all_sontarih.module);
+
+    console.log(toplam_ozkaynak_all_sontarih.then);
+    console.log(toplam_ozkaynak_all_sontarih.resolve);
+
+
+
+
+    
+
+    const allData = await toplam_ozkaynak_all_sontarih.innerHTML;
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    console.log("sdf",allData);
+
+
+    toplam_ozkaynak_all_sontarih.innerHTML.then(result => {
+      toplam_oazkayank_all_son_tarih_time=result;
+    })
+
+    console.log("---");
+    console.log(toplam_oazkayank_all_son_tarih_time)
+
+
+    let toplam_oazkayank_all_sontarih_value;
+    const toplam_ozkaynak_all_sontarihh=await driver.findElement(By.xpath(`//*[@id="${stock}"]/div[5]/div[1]/div/main/div/div/div[3]/div[4]/table[2]/tbody[1]/tr[2]/td[2]/div/span[2]`));
+    toplam_ozkaynak_all_sontarihh.getText().then(result => {
+      toplam_oazkayank_all_sontarih_value=result;
+    })
+
+    await new Promise(resolve => setTimeout(resolve, 1000));
+
+    return toplam_oazkayank_all_son_tarih_time;
+},
+
+
+
     toplam_oazkayank:async function(driver,stock) {
         driver.manage().window().maximize();
       
