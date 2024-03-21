b5d724b6d4f1f76df5d11af0403063decf3c93ea
berk gümüş <63772343+berk420@users.noreply.github.com>
2024-03-21 20:20:33 +0300

    update

--- StockMarketPrediction/test_test.js
+++ StockMarketPrediction/test_test.js
@@ -12,9 +12,10 @@ const {
   Nakit_ve_Nakit_Benzerleri,
   ticari_alacaklar,
   toplam_oazkayank_all,
-
   NakitveNakitBenzerleri_all} 
   = require("./test/financial_statement_values.js");
+
+
 
 
 async function testing(){
@@ -27,20 +28,94 @@ async function testing(){
 
   let NakitveNakitBenzerleri_allss=await NakitveNakitBenzerleri_all(driver,stock);
 
-  console.log(NakitveNakitBenzerleri_allss);
   for (let eleman of NakitveNakitBenzerleri_allss) {
     console.log(eleman);
+
   }
 
-  NakitveNakitBenzerleri_allss.forEach((satir) => {
-    let satirYazisi = "";
-    satir.forEach((eleman) => {
-      satirYazisi += eleman + " ";
-    });
-    console.log(satirYazisi);
+}
+
+function postcire(){
+
+  const{Client}=require('pg');
+  const client = new Client({
+    host:"localhost",
+    user:"postgres",
+    port:"5432",
+    password:"0606",
+    database:"dbstock"
   });
+
+  client.connect();
+  let  query = "INSERT INTO bilanco (hisse_adi, bilanco_kalemi, zamani, degeri) VALUES ('Hisse1', 'Kalemi1', '2024-03-16 10:00:00', 100), ('Hisse2', 'Kalemi2', '2024-03-16 11:00:00', 150), ('Hisse3', 'Kalemi3', '2024-03-16 12:00:00', 200);"
+  let  sildick="DELETE FROM bilanco";
+  client.query(sildick, (err,res)=>{
+    if(!err){
+      console.log(res.rows);
+    }else{
+      console.log(err.message);
+    }
+    client.end;
+  })
+
 
 
 }
 
-testing();
+//postcire();
+
+//testing();
+async function gunder() {
+  const { Client } = require('pg');
+  const client = new Client({
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "0606",
+    database: "dbstock"
+  });
+
+  try {
+    // Veritabanına bağlanma
+    await client.connect();
+    let driver = await new Builder().forBrowser('chrome').build();
+    stock="TUPRS"
+    await driver.get(`https://fintables.com/sirketler/TUPRS/finansal-tablolar/bilanco`);
+
+    let  data =await NakitveNakitBenzerleri_all(driver,stock);
+
+    // 2 boyutlu dizi
+
+    // Her bir veriyi tabloya eklemek için döngü
+    for (const item of data) {
+      const hisse_adi = 'TUPRS'; // Hisse adı sabit olarak 'TUPRS' olarak belirtildi
+      const bilanco_kalemi = 'Nakit ve Nakit Benzerleri'; // Bilanco kalemi sabit bir değer olarak belirtildi, gerçek veriye göre güncellenmeli
+      const zamani = item[0]; // Dizinin ilk elemanı 'zamani' sütununa atanıyor
+      const degeri = item[1]; // Dizinin ikinci elemanı 'degeri' sütununa atanıyor
+
+      // SQL sorgusu
+      const sql = `INSERT INTO bilanco (hisse_adi, bilanco_kalemi, zaman, deger) VALUES ($1, $2, $3, $4)`;
+
+      // Sorguyu çalıştırma
+      await client.query(sql, [hisse_adi, bilanco_kalemi, zamani, degeri]);
+      console.log("Veri başarıyla eklendi.");
+    }
+  } catch (err) {
+    console.error("Veri eklenirken hata oluştu:", err.message);
+  } finally {
+    // Veritabanı bağlantısını kapatma
+    await client.end();
+  }
+}
+
+// Fonksiyonu çağır
+gunder();
+
+
+
+
+
+
+
+
+
